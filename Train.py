ebac74e1318dbc034485b95cf238db9b0b926b13
Mohamed Ibrahim <mohmed42010@gmail.com>
2022-10-31 18:45:54 +0200

    some optimization in gui code

--- Train.py
+++ Train.py
@@ -1,5 +1,4 @@
 
-
 import random
 # Training model using signum function and perceptron learning algorithm
 def train_model(feature1_1, feature1_2, feature2_1, feature2_2, label1, label2, LearningRate, Epochs, useBias):
@@ -10,17 +9,14 @@ def train_model(feature1_1, feature1_2, feature2_1, feature2_2, label1, label2, 
     random.shuffle(input_feature)
     for epoch in range(Epochs):
         for i in range(len(input_feature)):
-            feature0 = float(input_feature[i][0])
-            feature1 = float(input_feature[i][1])
-            feature2 = float(input_feature[i][2])
-            net = feature0 * weight1 + feature1 * weight2 + bias * float(useBias)
+            net = input_feature[i][0] * weight1 + input_feature[i][1] * weight2 + bias * float(useBias)
             if net > 0:
                 y = 1
             else:
                 y = -1
-            loss = feature2 - y
-            weight1  = weight1 + LearningRate * loss * feature0
-            weight2 = weight2 + LearningRate * loss * feature1 
+            loss = input_feature[i][2] - y
+            weight1  = weight1 + LearningRate * loss * input_feature[i][0]
+            weight2 = weight2 + LearningRate * loss * input_feature[i][1] 
             bias = bias + LearningRate * loss * float(useBias)
     
     
--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from Visualization import *
 # feature1_2 = data['bill_depth_mm'][:30]
 # feature2_1 =  data['bill_length_mm'][50:80]
 # feature2_2 = data['bill_depth_mm'][50:80]
-preVis()
-combo, combo2, species1,species2,learningRate,epochs,useBias = startGUI()
+# preVis()
+combo, combo2, species1,species2, learningRate, epochs, useBias = startGUI()
 feature1_1 = []
 feature1_2 = []
 feature2_1 = []
@@ -22,13 +22,14 @@ learningRate = float(learningRate) if learningRate != '' else 0.01
 epochs = int(epochs) if epochs != '' else 100
 useBias = useBias if useBias != '' else 0
 
-print("Feature1: "+combo)
-print("Feature2: "+combo2)
-print("species1: "+species1)
-print("species1: "+species2)
-print("learningRate: "+str(learningRate))
-print("Epochs: "+str(epochs))
-print("useBias: "+str(useBias))
+# print("Feature1: "+combo)
+# print("Feature2: "+combo2)
+# print("species1: "+species1)
+# print("species1: "+species2)
+# print("learningRate: "+str(learningRate))
+# print("Epochs: "+str(epochs))
+# print("useBias: "+str(useBias))
+
 for i in range(len(data)):
     if data['species'][i] == species1:
         feature1_1.append(data[combo][i])
@@ -37,11 +38,17 @@ for i in range(len(data)):
     if data['species'][i] == species2:
         feature2_1.append(data[combo][i])
         feature2_2.append(data[combo2][i])
-
+# convert the data to float
+feature1_1 = [float(feature1_1[x]) for x in range(len(feature1_1))]
+feature1_2 = [float(feature1_2[x]) for x in range(len(feature1_1))]
+feature2_1 = [float(feature2_1[x]) for x in range(len(feature1_1))]
+feature2_2 = [float(feature2_2[x]) for x in range(len(feature1_1))]
+# divide the data to train and test
 feature1_1, test1_1 = feature1_1[:30], feature1_1[30:]
 feature1_2, test1_2 = feature1_2[:30], feature1_2[30:]
 feature2_1, test2_1 = feature2_1[:30], feature2_1[30:]
 feature2_2, test2_2 = feature2_2[:30], feature2_2[30:]
+
 
 
 # get first 2 labels
@@ -52,7 +59,6 @@ label2 = [-1.0] * 30
 visualize(feature1_1, feature1_2, feature2_1, feature2_2, 0, 0, 0, combo,combo2, species1, species2)
 # train the model
 weight1, weight2, bias = train_model(feature1_1, feature1_2, feature2_1, feature2_2, label1, label2, learningRate,epochs,useBias)
-print("Bias: "+str(bias))
 # Accuracy on training data
 print("Train Accuracy: ", test_model(weight1, weight2, feature1_1, feature1_2, feature2_1, feature2_2, label1, label2, bias) * 100, "%")
 # visualize the decision boundary
